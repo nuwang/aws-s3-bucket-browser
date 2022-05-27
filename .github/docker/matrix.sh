dce3259189701f83c501fae3224e70eb559fb3cc
Nuwan Goonasekera <2070605+nuwang@users.noreply.github.com>
2022-05-27 18:00:37 +0530

    Switch to cvmfs folder bfore searching

--- .github/docker/matrix.sh
+++ .github/docker/matrix.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-find /cvmfs/data.galaxyproject.org/ -maxdepth 2 | sort | awk '$0 !~ last "/" {print last} {last=$0} END {print last}' | grep "\S" | jq -R '[.]' | jq -s -c 'add'
+cd /cvmfs/data.galaxyproject.org/
+find . -maxdepth 2 | sort | awk '$0 !~ last "/" {print last} {last=$0} END {print last}' | grep "\S" | jq -R '[.]' | jq -s -c 'add'
