7e0bcb5286e5a50f32c0e118ad0c28619c6696d5
Nuwan Goonasekera <2070605+nuwang@users.noreply.github.com>
2022-05-27 19:48:26 +0530

    Misc. fixes to matrix and sync scripts

--- .github/docker/matrix.sh
+++ .github/docker/matrix.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-cd /cvmfs/data.galaxyproject.org/
-find . -maxdepth 2 | sort | awk '$0 !~ last "/" {print last} {last=$0} END {print last}' | grep "\S" | jq -R '[.]' | jq -s -c 'add'
+# Find all subdirectories at depth 2 and sort them, use awk to remove any parent directories, use cut to remove the
+# prefix (/cvmfs/data.galaxyproject.org), use grep to remove empty lines and use jq to convert the resultant list
+# to a javascript array of directories.
+find /cvmfs/data.galaxyproject.org/ -maxdepth 2 -type d | sort | awk '$0 !~ last "/" {print last} {last=$0} END {print last}' | cut -c 31- | grep "\S" | jq -R '[.]' | jq -s -c 'add'
--- .github/docker/sync.sh
+++ .github/docker/sync.sh
@@ -6,9 +6,9 @@ set -xe
 /app/mount.sh
 
 printf \nMirroring cvmfs to s3
-rsync --progress -avh --exclude=".*" /cvmfs/data.galaxyproject.org/$1 /opt/s3fs/galaxy/v1/data.galaxyproject.org/$1
+rsync --progress -avh --exclude=".*" /cvmfs/data.galaxyproject.org/$1/ /opt/s3fs/galaxy/v1/data.galaxyproject.org/$1/
 
 printf \nMake all symlinks geesefs compatible
-find /opt/s3fs/galaxy/v1/data.galaxyproject.org/$1 -type l -ls | awk '{print substr($11, 11) " " $13}'  | xargs -l bash -c 's3cmd modify --add-header "x-amz-meta---symlink-target:$1" s3://biorefdata/$0'
+find /opt/s3fs/galaxy/v1/data.galaxyproject.org/$1 -type l -ls | awk '{print substr($11, 11) " " $13}'  | xargs -l bash -c 's3cmd modify --add-header "x-amz-meta---symlink-target:$1" s3://biorefdata/$0' || echo "No files found or error"
 
 printf Done!
