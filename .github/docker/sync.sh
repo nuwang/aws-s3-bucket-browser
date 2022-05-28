008a80e12ed02c11db6c9730712a83815e8c0f26
Nuwan Goonasekera <2070605+nuwang@users.noreply.github.com>
2022-05-28 21:00:24 +0530

    Some path fixes for rsync

--- .github/docker/sync.sh
+++ .github/docker/sync.sh
@@ -6,7 +6,9 @@ set -xe
 /app/mount.sh
 
 printf \nMirroring cvmfs to s3
-ls /cvmfs/data.galaxyproject.org/$1/ | xargs -n1 -P8 -I% rsync --progress -avh --exclude=".*" % /opt/s3fs/galaxy/v1/data.galaxyproject.org/$1/
+export CVMFS_PATH=/cvmfs/data.galaxyproject.org/$1
+export S3FS_PATH=/opt/s3fs/galaxy/v1/data.galaxyproject.org/$1
+ls $CVMFS_PATH | xargs -n1 -P8 -I% rsync --progress -avh --exclude=".*" $CVMFS_PATH/%/ $S3FS_PATH/%/ -delete
 
 printf \nMake all symlinks geesefs compatible
 find /opt/s3fs/galaxy/v1/data.galaxyproject.org/$1 -type l -ls | awk '{print substr($11, 11) " " $13}'  | xargs -n2 -P8 bash -c 's3cmd modify --add-header "x-amz-meta---symlink-target:$1" s3://biorefdata/$0' || echo "No files found or error"
