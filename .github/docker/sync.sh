746878b9649f49617fb8e7a11c31d9b696ba5326
Nuwan Goonasekera <2070605+nuwang@users.noreply.github.com>
2022-05-29 10:12:15 +0530

    Fix matrix path and some minor path tweaks

--- .github/docker/sync.sh
+++ .github/docker/sync.sh
@@ -8,7 +8,7 @@ set -xe
 printf \nMirroring cvmfs to s3
 export CVMFS_PATH=/cvmfs/data.galaxyproject.org/$1
 export S3FS_PATH=/opt/s3fs/galaxy/v1/data.galaxyproject.org/$1
-ls $CVMFS_PATH | xargs -n1 -P8 -I% rsync --progress -avh --exclude=".*" $CVMFS_PATH/%/ $S3FS_PATH/%/ -delete
+ls $CVMFS_PATH | xargs -n1 -P8 -I% rsync --progress -avh --exclude=".*" $CVMFS_PATH/% $S3FS_PATH/ -delete
 
 printf \nMake all symlinks geesefs compatible
 find /opt/s3fs/galaxy/v1/data.galaxyproject.org/$1 -type l -ls | awk '{print substr($11, 11) " " $13}'  | xargs -n2 -P8 bash -c 's3cmd modify --add-header "x-amz-meta---symlink-target:$1" s3://biorefdata/$0' || echo "No files found or error"
